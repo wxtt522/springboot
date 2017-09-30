6e2b7f88bda456c7939102decc160615a1d7f09e
wulh522 <wulonghui522@dingtalk.com>
2017-09-30 11:28:27 +0800

    去除不必要注解，添加请求参数拦截

--- src/main/java/study/dao/SheepMapper.java
+++ src/main/java/study/dao/SheepMapper.java
@@ -4,7 +4,6 @@ import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
-import org.springframework.stereotype.Repository;
 import study.entity.Sheep;
 
 import java.util.List;
@@ -12,10 +11,10 @@ import java.util.List;
 /**
  * Created by Administrator on 2017/8/17.
  */
-@Repository
-    public interface SheepMapper {
+public interface SheepMapper {
     @Insert("insert into sheep(name,age,sex) values(#{name},#{age},#{sex}) ")
-    @Options(useGeneratedKeys = true,keyColumn = "id",keyProperty = "id")   //id自增长
+    @Options(useGeneratedKeys = true, keyColumn = "id", keyProperty = "id")
+        //id自增长
     void save(Sheep sheep);
 
     @Delete("delete *from sheep where id =#{id}")
--- src/main/java/study/interceptor/logInterceptor.java
+++ src/main/java/study/interceptor/logInterceptor.java
@@ -2,17 +2,16 @@ package study.interceptor;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Enumeration;
 
 /**
  * Created by Administrator on 2017/9/30.
  */
-@Component
 public class logInterceptor implements HandlerInterceptor {
 
     Logger logger = LoggerFactory.getLogger(logInterceptor.class);
@@ -20,8 +19,24 @@ public class logInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         logger.info("into preHandle");
-        logger.info("url:{}",request.getRequestURL());
+        String requestUrl=request.getRequestURI();
+        String method = request.getMethod();
+        String params = getParams(request);
+
+        logger.info("请求路径requestUrl:{}, -提交类型 method:{}"+",-请求参数 paramList :{" +params+ "}",requestUrl,method);
         return true;
+    }
+
+    //获取请求参数
+    private String getParams(HttpServletRequest request){
+
+        StringBuilder sb = new StringBuilder();
+        Enumeration<String> enu = request.getParameterNames();// 类型的枚举
+        while (enu.hasMoreElements()) {
+            String name = enu.nextElement();
+            sb.append(name + "=" + request.getParameter(name) );
+        }
+        return sb.toString();
     }
 
     @Override
