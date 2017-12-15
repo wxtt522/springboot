ee2bb2ab1e2fdbe20ab678a7710783a783960a05
wulh522 <wulonghui522@dingtalk.com>
2017-12-15 11:54:32 +0800

    move with

--- src/main/java/study/commons/OSSClientConstants.java
+++ src/main/java/study/commons/OSSClientConstants.java
@@ -8,15 +8,15 @@ package study.commons;
  */
 public class OSSClientConstants {
     //阿里云API的外网域名
-    public static final String ENDPOINT = "http://oss-cn-hangzhou.aliyuncs.com";
+    public static final String ENDPOINT = "";
     //阿里云API的密钥Access Key ID
-    public static final String ACCESS_KEY_ID = "LTAIy3NicWl6LleZ";
+    public static final String ACCESS_KEY_ID = "";
     //阿里云API的密钥Access Key Secret
-    public static final String ACCESS_KEY_SECRET = "B66HABrHSrYMc5o5kUOizkpjO7BVOM";
+    public static final String ACCESS_KEY_SECRET = "";
     //阿里云API的bucket名称
-    public static final String BACKET_NAME = "htaocar";
+    public static final String BACKET_NAME = "";
     //阿里云API的文件夹名称
-    public static final String FOLDER="car/";
+    public static final String FOLDER="";
 
 
 }
